99700775a2754944aef97c134c4b91751eb7590d
mbomak <mboma@meta.ua>
2017-01-18 13:36:19 +0200

    oop

--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -1,224 +1,216 @@
 "use strict";
 ( function(){
 
-	class Drawing{
+	class Shape {
+
+		constructor( x,y ){
+			this.x = x;
+			this.y = y;
+			this.canvas = document.createElement( 'canvas' );
+			this._ctx = this.canvas.getContext( '2d' );
+			this._color = this._getRandomColor();
+			this._widthCanvas = 60;
+			this._heightCanvas = 60;
+			this._setCanvasSize();
+			this._drawShape();
+		}
+
+		_getRandomColor(){
+
+			let letters = '0123456789ABCDEF';
+
+			let color = '#';
+
+			for (var i = 0; i < 6; i++ ) {
+
+			    color += letters[Math.floor(Math.random() * 16)];
+
+			}
+
+			return color;
+
+		}
+
+		_setCanvasSize(){
+
+			this.canvas.width = this._widthCanvas;
+			this.canvas.height = this._heightCanvas;
+
+		}
+	}
+
+	class Triangle extends Shape {
+
+		_drawShape(){
+
+			let triangleHeight = this._heightCanvas*(1.5/Math.sqrt(3));
+
+			this._ctx.beginPath();
+			this._ctx.fillStyle = this._color;
+			this._ctx.lineJoin = 'miter';
+			this._ctx.moveTo( this._widthCanvas/2,0 );
+			this._ctx.lineTo( this._widthCanvas,triangleHeight );
+			this._ctx.lineTo( 0,triangleHeight );
+			this._ctx.closePath();
+			this._ctx.fill();
+
+		}
+	
+	}
+
+	class Square extends Shape {
+
+		_drawShape(){
+
+				this._ctx.beginPath();
+				this._ctx.fillStyle = this._color;
+				this._ctx.rect( 0,0,this._widthCanvas,this._heightCanvas );
+				this._ctx.fill();
+
+		}
+
+	}
+
+	class Circle extends Shape {
+
+		_drawShape(){
+
+				this._ctx.beginPath();
+				this._ctx.fillStyle = this._color;
+				this._ctx.arc( 
+					this._widthCanvas/2,
+					this._heightCanvas/2,
+					this._widthCanvas/2,
+					0,
+					2*Math.PI,
+					false
+				);
+
+				this._ctx.fill();
+		}
+	
+	}
+
+	class Game{
 
 		constructor( item ){
 			//private properties
 			this._canvas = item;
 			this._ctx = this._canvas.getContext('2d');
-			this._canvasCur = undefined;
-			this._pos = undefined;
 			this._shape = undefined;
-			this._color = undefined;
-			this._arrCanvas = [];
+			this._shapes = [];
 			this._radioButtons = document.querySelectorAll( '.site__shape input' );
+
+			this._takeShape();
 			this._onEvents();
+			this._loop();
 		}
 
 		_onEvents(){
 
-			let getPos = ( evt )=>{
+			this._onShapeChange();
 
-			  	let rect = this._canvas.getBoundingClientRect();
+			this._canvas.addEventListener( 'mousedown',( event )=>{
 
-			  	return  {
-			  		x: Math.round(evt.clientX - rect.left),
-			  		y: Math.round(evt.clientY - rect.top)
-			  	};
+				this._canvas.addEventListener ( 'mousemove', ()=>{
 
-			},
-			takeShape = ()=>{
+					console.log('move');
 
-			  let checkedButton = undefined;
+				});
 
-			  for( let item of this._radioButtons ){
+				let position = this._getMousePosition ( event );	
+				
+				let x = position.x;			
+				let y = position.y;
 
-			  	if ( item.checked ) checkedButton = item;
+				if ( this._shape == 'square' )	{
 
-			  }
+					this._shapes.push( new Square( x,y ) );
 
-			  if ( checkedButton.value == 1 ) {
+				}	else if ( this._shape == 'triangle' ) {
 
-			  	 this._shape = 'square';
+					this._shapes.push( new Triangle( x,y ) );
 
-			  } else if ( checkedButton.value == 2 ) {
+				} else if ( this._shape == 'circle' ) {
 
-			  	this._shape = 'triangle';
+					this._shapes.push( new Circle( x,y ) );
 
-			  } else if ( checkedButton.value == 3 ) {
+				}
 
-			  	this._shape = 'circle';
+			} );	
 
-			  }
+		}
 
-			},
-			changeShape = ()=>{
+		_getMousePosition( evt ){
 
-			  for( let item of this._radioButtons ){
+			let rect = this._canvas.getBoundingClientRect();
 
-			  	item.addEventListener( 'change', ()=>{
+			return  {
+				x: Math.round(evt.clientX - rect.left),
+				y: Math.round(evt.clientY - rect.top)
+			};
 
-			  		takeShape();
+		}
 
-			  	} );
+		_takeShape(){
 
-			  }
+			for( let item of this._radioButtons ){
 
-			},
-			getRandomColor = ()=>{
+				if ( item.checked ) {
 
-			  let letters = '0123456789ABCDEF';
+					this._shape = item.value;
 
-			  let color = '#';
+				}
+			}			
+		}
 
-			  for (var i = 0; i < 6; i++ ) {
+		_onShapeChange(){
 
-			      color += letters[Math.floor(Math.random() * 16)];
+			for( let item of this._radioButtons ){
 
-			  }
+				item.addEventListener( 'change', ()=>{
 
-			  return color;
-
-			},
-			drawSquare = ( color )=>{
-
-				let ctx = initNewCanvas( 60,60 );
-
-				ctx.beginPath();
-				ctx.fillStyle = color;
-				ctx.rect(0,0,60,60);
-				ctx.fill();
-
-			},
-			drawTriangle = ( color)=>{
-
-				let heightCanv = 90/Math.sqrt(3);
-
-				let ctx = initNewCanvas( 60,heightCanv );
-
-				ctx.beginPath();
-				ctx.fillStyle = color;
-				ctx.lineJoin = 'miter';
-				ctx.moveTo( 30,0 );
-				ctx.lineTo( 60,heightCanv );
-				ctx.lineTo( 0,heightCanv );
-				ctx.closePath();
-				ctx.fill();
-
-			},
-			drawCircle = ( color )=>{
-
-				let ctx = initNewCanvas( 60,60 );
-
-				ctx.beginPath();
-				ctx.fillStyle = color;
-				ctx.arc( 30,30,30,0,2*Math.PI,false );
-				ctx.fill();
-
-			},
-			initNewCanvas = ( width,height )=>{
-
-				let canvas = undefined;
-				let context = undefined; 
-
-				canvas = document.createElement( 'canvas' );
-
-				canvas.width = width;
-				canvas.height = height;
-
-				this._canvasCur = canvas;
-
-				context = canvas.getContext('2d');
- 
-				return context;
-
-			},
-			clickOnCanvas = ()=>{
-
-				this._canvas.addEventListener( 'click',( e )=>{
-
-					if ( this._arrCanvas.length !=0 ) {
-
-						// for ( let i = 0; i < this._arrCanvas.length; i++ ) {
-
-						// 	let contCanvasArr = this._arrCanvas[i];
-
-						// 	if ( contCanvasArr.isPointInPath(30,30) ) {
-
-						// 		draw( e );
-
-						// 	} else {
-
-						// 		draw( e );
-
-						// 	}
-						// }
-
-						draw( e );
-					} else {
-
-						draw( e );
-						
-					}
+					this._takeShape();
 
 				} );
-			
-			},
-			draw = ( e )=>{
 
-				if ( this._ctx ) {
+			}
 
-					this._pos = getPos( e );
-					let x = this._pos.x;
-					let y = this._pos.y;
+		}
 
-					this._color = getRandomColor();
+		_loop(){
 
-					if ( this._shape == 'square' ) {
+			let loop = ()=>{
 
-						drawSquare( this._color );
-
-						this._ctx.drawImage( this._canvasCur,x-30,y-30 );
-
-						this._arrCanvas.push(this._canvasCur.getContext('2d'));
-
-					} else if ( this._shape == 'triangle' ) {
-
-						drawTriangle( this._color );
-
-						this._ctx.drawImage( this._canvasCur,x-30,y-(60/Math.sqrt(3)) );
-
-						this._arrCanvas.push(this._canvasCur);
-
-					} else if ( this._shape == 'circle' ) {
-
-						drawCircle( this._color );
-
-						this._ctx.drawImage( this._canvasCur,x-30,y-30 );
-
-						this._arrCanvas.push(this._canvasCur);
-
-					}
-				}
+				this._draw();
+				requestAnimationFrame( loop );
 
 			};
 
-			takeShape();
+			loop();
 
-			changeShape();
+		}
 
-			clickOnCanvas();
+		_draw(){
+
+			this._ctx.clearRect( 0,0,this._canvas.width,this._canvas.height );
+
+			this._shapes.forEach( ( item )=>{
+
+				this._ctx.save();
+				this._ctx.translate( item.x, item.y );
+				this._ctx.drawImage( item.canvas,-30,-30 );
+				this._ctx.restore();
+
+			} );
 
 		}
 
 	}
 
-
 	for( let item of document.querySelectorAll( '.canvas' ) ){
 
-		new Drawing( item );
+		new Game( item );
 
 	}
-
 } )();
