3e8e3a6bfd7df89e5b39d8c0cf6e878e252b4080
unknown <k.oliferchuk@digitalwand.ru>
2017-05-02 23:38:45 +0300

    start

--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -1,4 +1,60 @@
 "use strict";
 ( function(){
 
+    $( '.popup-buggy-feedback__form' ).validate({
+        ignore: [], 
+        rules: {
+            popupBuggyFeedbackEmail: {
+                required: true,
+                email: true
+            }           
+        },
+        messages: {
+            popupBuggyFeedbackEmail: {
+                required: 'Введите, пожалуйста, Ваш e-mail!',
+                email: 'Введите, пожалуйста, правельный e-mail!'
+            }
+        },
+        errorPlacement: function(error, element) {
+            error.insertBefore(element);
+        }
+    });
+
+        $.validator.addMethod("valueNotEquals", function(value, element, arg){
+            return arg != value;
+        }, "Value must not equal arg.");
+
+    
+
+        $( '.popup-places__form' ).validate({
+            rules: {
+                placesName: {
+                    required: true
+               },
+                popupPlacesService1: {
+                    valueNotEquals: "default"
+                },
+                popupPlacesCity: {
+                    valueNotEquals: "default"
+                },
+                popupPlacesAdress: {
+                    required: true
+                }            
+            },
+            messages: {
+                placesName: {
+                    required: 'Введите пожалуйста название места!'
+                },
+                popupPlacesService1: {
+                    valueNotEquals: 'Выбирите пожалуйста услугу!'                 
+                },
+                popupPlacesCity: {
+                    valueNotEquals: 'Укажите город!'               
+                },
+                popupPlacesAdress: {
+                    required: 'Укажите адрес!'
+                }
+            }
+        });
+
 } )();
